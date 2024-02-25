da2855833c400b4414c4dc5190e08eecdb29124d
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-02-25 21:00:18 +0700

    phân quyền (khó)

--- src/components/auth/AclGuard.tsx
+++ src/components/auth/AclGuard.tsx
@@ -2,7 +2,12 @@
 import { ReactNode } from 'react'
 
 // ** Types
-import type { ACLObj } from 'src/configs/acl'
+import { buildAbilityFor, type ACLObj, AppAbility } from 'src/configs/acl'
+import BlankLayout from 'src/views/layout/BlankLayout'
+import NotAuthorized from 'src/pages/401'
+import { useAuth } from 'src/hooks/useAuth'
+import { useRouter } from 'next/router'
+import { AbilityContext } from '../acl/Can'
 
 interface AclGuardProps {
   children: ReactNode
@@ -13,10 +18,39 @@ interface AclGuardProps {
 
 const AclGuard = (props: AclGuardProps) => {
   // ** Props
-  // const { aclAbilities, children, guestGuard = false, authGuard = true } = props
-  const { children } = props
+  const { aclAbilities, children, guestGuard = false, authGuard = true } = props
+  const router = useRouter()
+  const auth = useAuth()
+  const permissionUser = auth.user?.role.permissions ?? []
+  let ability: AppAbility
+  if (auth.user && !ability) {
+    ability = buildAbilityFor(permissionUser, aclAbilities.subject)
+  }
+  // console.log(ability) //đọc sẽ ko hiểu gì đâu =)) do nó ở dnagj map
+  //khi trả về thăng casl => sử dụng cái ab.can để check quyền
 
-  return <>{children}</>
+  //nếu guestG or ko đăng nhập và những pge lỗi thì ko cần phân quyền
+  if (guestGuard || router.route === '/500' || router.route === '/404' || !authGuard) {
+    //ko cần phải check quyền
+    if (auth.user && ability) {
+      //đã đnhap và đã build quyền rồi
+      return <AbilityContext.Provider value={ability}>{children}</AbilityContext.Provider>
+    } else {
+      //chưa đăng nhập và ch có ab => thì ch có quyền để mà check như trên
+      return children
+    }
+  }
+
+  // những cái cần check quyền
+  if (ability && auth.user && ability.can(aclAbilities.action, aclAbilities.subject)) {
+    return <AbilityContext.Provider value={ability}>{children}</AbilityContext.Provider>
+  }
+
+  return (
+    <BlankLayout>
+      <NotAuthorized></NotAuthorized>
+    </BlankLayout>
+  )
 }
 
 export default AclGuard
--- src/configs/acl.ts
+++ src/configs/acl.ts
@@ -16,22 +16,20 @@ export type ACLObj = {
  * We have just shown Admin and Client rules for demo purpose where
  * admin can manage everything and client can just visit ACL page
  */
-const defineRulesFor = (role: string, subject: string) => {
+const defineRulesFor = (permission: string[], subject: string) => {
   const { can, rules } = new AbilityBuilder(AppAbility)
 
-  if (role === 'admin') {
+  if (permission.includes('ADMIN.GRANTED')) {
     can('manage', 'all')
-  } else if (role === 'client') {
-    can(['read'], 'acl-page')
-  } else {
-    can(['read', 'create', 'update', 'delete'], subject)
   }
 
   return rules
 }
-
-export const buildAbilityFor = (role: string, subject: string): AppAbility => {
-  return new AppAbility(defineRulesFor(role, subject), {
+//dùng để trả veef 1 instance của appAplity dựa vào per và subject đã cung cấp cho nó
+// sau đó dựa vào defineRulesFor để tạo quyền cho nó
+//nếu là can thì là có quyền còn cannot là ko có quyền
+export const buildAbilityFor = (permission: string[], subject: string): AppAbility => {
+  return new AppAbility(defineRulesFor(permission, subject), {
     // https://casl.js.org/v5/en/guide/subject-type-detection
     // @ts-ignore
     detectSubjectType: object => object!.type
