06f17b36101d100ab22c2bb40782f54a26db3bfe
Tran Duc Manh <manhtranduc0202@gmail.com>
2024-04-03 14:50:26 +0700

    custom modal

---
+++ src/components/custom-modal/index.tsx
@@ -0,0 +1,56 @@
+import Modal, { ModalProps } from '@mui/material/Modal'
+import { styled } from '@mui/material'
+import { Box } from '@mui/material'
+
+interface TCustomModal extends ModalProps {
+  handleClose: () => void
+}
+
+const StyleModal = styled(Modal)<ModalProps>(({ theme }) => ({
+  zIndex: 1200,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+}))
+
+const CustomModal = (props: TCustomModal) => {
+  const { children, handleClose, open } = props
+
+  return (
+    <StyleModal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby='modal-modal-title'
+      aria-describedby='modal-modal-description'
+    >
+      <Box
+        sx={{
+          height: '100%',
+          width: '100vw'
+        }}
+      >
+        <Box sx={{ maxHeight: '100vh', overflow: 'auto' }}>
+          <Box
+            sx={{
+              height: '100%',
+              width: '100%',
+              minHeight: '100vh',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Box
+              sx={{
+                margin: '40px 0'
+              }}
+            >
+              {children}
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </StyleModal>
+  )
+}
+export default CustomModal
