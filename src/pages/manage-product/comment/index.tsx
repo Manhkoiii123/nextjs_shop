b3ca96ec9c6fc789e31b094ba67ca6570e7ee6fe
padi-dev-manhtd <manhtd@paditech.com>
2024-09-29 16:34:57 +0700

    cms comment done

---
+++ src/pages/manage-product/comment/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from 'next'
+import CommentListPage from 'src/views/pages/manage-product/comment/CommentList'
+
+type TProps = {}
+
+const Index: NextPage<TProps> = () => {
+  return <CommentListPage />
+}
+
+export default Index
